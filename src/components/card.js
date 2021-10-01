4e75eb1f217ee504c8c5c47f9ff937065305da14
Hughes <hughespham2002the2nd@yahoo.com>
2021-10-01 18:33:01 -0500

    done

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +18,60 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const mainCard = document.createElement('div');
+  const cardHead = document.createElement('div');
+  const cardAuthor = document.createElement('div');
+  const cardImgContain = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardName = document.createElement('span');
+
+  mainCard.classList.add('card');
+  cardHead.classList.add('headline');
+  cardAuthor.classList.add('author');
+  cardImgContain.classList.add('img-container');
+
+  cardHead.textContent = article['headline'];
+  cardName.textContent = `By ${article.authorName}`;
+  cardImg.src = article['authorPhoto'];
+
+  mainCard.appendChild(cardHead);
+  mainCard.appendChild(cardAuthor);
+  cardAuthor.appendChild(cardImgContain);
+  cardImgContain.appendChild(cardImg);
+  cardAuthor.appendChild(cardName);
+  
+  mainCard.addEventListener('click', ()=>{
+  console.log(article.headline);
+});
+console.log(mainCard);
+  return mainCard
+  
+// const fCard = document.createElement('div');
+//   const fHeadline = document.createElement('div');
+//   const fAuthor = document.createElement('div');
+//   const fImgContainer = document.createElement('div');
+//   const fImg = document.createElement('img');
+//   const fAuthorName = document.createElement('span');
+
+//   fCard.classList.add('card');
+//   fHeadline.classList.add('headline');
+//   fAuthor.classList.add('author');
+//   fImgContainer.classList.add('img-container');
+  
+//   fHeadline.textContent = article.headline;
+//   fImg.src = article.authorPhoto;
+//   fAuthorName.textContent =`By ${article.authorName}`;
+
+//   fCard.appendChild(fHeadline);
+//   fCard.appendChild(fAuthor);
+//   fAuthor.appendChild(fImgContainer);
+//   fImgContainer.appendChild(fImg);
+//   fAuthor.appendChild(fAuthorName);
+
+//   fCard.addEventListener('click', function() {
+//     console.log(article.headline);
+//   });
+//   return fCard;
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +83,20 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  let response = axios.get(`http://localhost:5000/api/articles`)
+
+response.then(response => {
+
+  let list = response.data.articles;
+
+  for(let data in list) {
+    list[data].forEach(element => {
+      let card1 = Card(element);
+      let cards = document.querySelector(selector);
+      cards.appendChild(card1);
+    });
+  }
+}).catch (function (error) {console.log(error)})
 }
 
 export { Card, cardAppender }
